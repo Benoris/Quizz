961cc229a575f73e0642c5da361e10e0894e100e
psccc97 <pascucci.lino@gmail.com>
2016-10-12 16:03:22 +0200

    Modification de la form Source

--- Score.Designer.cs
+++ Score.Designer.cs
@@ -28,20 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Score));
-            this.lbx_ListeJoueur = new System.Windows.Forms.ListBox();
+            this.lsb_ListeJoueur = new System.Windows.Forms.ListBox();
             this.lbl_ListeJoueur = new System.Windows.Forms.Label();
-            this.lbl_Score = new System.Windows.Forms.Label();
+            this.btn_QuitterScore = new System.Windows.Forms.Button();
             this.lbl_NumScore = new System.Windows.Forms.Label();
+            this.lbl_Score = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lbx_ListeJoueur
+            // lsb_ListeJoueur
             // 
-            this.lbx_ListeJoueur.FormattingEnabled = true;
-            this.lbx_ListeJoueur.Location = new System.Drawing.Point(12, 32);
-            this.lbx_ListeJoueur.Name = "lbx_ListeJoueur";
-            this.lbx_ListeJoueur.Size = new System.Drawing.Size(179, 277);
-            this.lbx_ListeJoueur.TabIndex = 0;
+            this.lsb_ListeJoueur.FormattingEnabled = true;
+            this.lsb_ListeJoueur.Location = new System.Drawing.Point(12, 32);
+            this.lsb_ListeJoueur.Name = "lsb_ListeJoueur";
+            this.lsb_ListeJoueur.Size = new System.Drawing.Size(179, 277);
+            this.lsb_ListeJoueur.TabIndex = 0;
+            this.lsb_ListeJoueur.SelectedIndexChanged += new System.EventHandler(this.lsb_ListeJoueur_SelectedIndexChanged);
             // 
             // lbl_ListeJoueur
             // 
@@ -52,38 +53,48 @@
             this.lbl_ListeJoueur.TabIndex = 2;
             this.lbl_ListeJoueur.Text = "La liste des joueurs";
             // 
-            // lbl_Score
+            // btn_QuitterScore
             // 
-            this.lbl_Score.AutoSize = true;
-            this.lbl_Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Score.Location = new System.Drawing.Point(211, 101);
-            this.lbl_Score.Name = "lbl_Score";
-            this.lbl_Score.Size = new System.Drawing.Size(59, 20);
-            this.lbl_Score.TabIndex = 3;
-            this.lbl_Score.Text = "Score :";
+            this.btn_QuitterScore.Location = new System.Drawing.Point(238, 286);
+            this.btn_QuitterScore.Name = "btn_QuitterScore";
+            this.btn_QuitterScore.Size = new System.Drawing.Size(75, 23);
+            this.btn_QuitterScore.TabIndex = 5;
+            this.btn_QuitterScore.Text = "Quitter";
+            this.btn_QuitterScore.UseVisualStyleBackColor = true;
             // 
             // lbl_NumScore
             // 
             this.lbl_NumScore.AutoSize = true;
             this.lbl_NumScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_NumScore.Location = new System.Drawing.Point(288, 101);
+            this.lbl_NumScore.Location = new System.Drawing.Point(287, 71);
             this.lbl_NumScore.Name = "lbl_NumScore";
             this.lbl_NumScore.Size = new System.Drawing.Size(42, 20);
             this.lbl_NumScore.TabIndex = 4;
             this.lbl_NumScore.Text = "Num";
+            // 
+            // lbl_Score
+            // 
+            this.lbl_Score.AutoSize = true;
+            this.lbl_Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Score.Location = new System.Drawing.Point(210, 71);
+            this.lbl_Score.Name = "lbl_Score";
+            this.lbl_Score.Size = new System.Drawing.Size(59, 20);
+            this.lbl_Score.TabIndex = 3;
+            this.lbl_Score.Text = "Score :";
             // 
             // Score
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(350, 331);
+            this.Controls.Add(this.btn_QuitterScore);
             this.Controls.Add(this.lbl_NumScore);
             this.Controls.Add(this.lbl_Score);
             this.Controls.Add(this.lbl_ListeJoueur);
-            this.Controls.Add(this.lbx_ListeJoueur);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Controls.Add(this.lsb_ListeJoueur);
             this.Name = "Score";
             this.Text = "Score";
+            this.Load += new System.EventHandler(this.Score_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,9 +102,10 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox lbx_ListeJoueur;
+        private System.Windows.Forms.ListBox lsb_ListeJoueur;
         private System.Windows.Forms.Label lbl_ListeJoueur;
-        private System.Windows.Forms.Label lbl_Score;
+        private System.Windows.Forms.Button btn_QuitterScore;
         private System.Windows.Forms.Label lbl_NumScore;
+        private System.Windows.Forms.Label lbl_Score;
     }
 }
--- Score.cs
+++ Score.cs
@@ -12,9 +12,31 @@ namespace Projet_Quizz
 {
     public partial class Score : Form
     {
+        List<string> list = new List<string>();
+        List<List<string>> list2 = new List<List<string>>();
         public Score()
         {
             InitializeComponent();
+        }
+
+        private void Score_Load(object sender, EventArgs e)
+        {
+            list2.Add(new List<string> { "Leila", "30" });
+            list2.Add(new List<string> { "Hulk", "50" });
+            list2.Add(new List<string> { "Tony", "100" });
+            list2.Add(new List<string> { "Bob", "48" });
+            
+            for (int i = 0; i < list2.Count; i++)
+            {
+                lsb_ListeJoueur.Items.Add(list2[i][0]);
+            }
+            
+
+        }
+
+        private void lsb_ListeJoueur_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbl_NumScore.Text = list2[lsb_ListeJoueur.SelectedIndex][1];
         }
     }
 }
