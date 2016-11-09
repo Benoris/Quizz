29faba08df4ea090f46ce6976915917639c6eb5e
Dean <maurice.dinh@gmail.com>
2016-11-09 15:57:29 +0100

    Commit 15

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -184,6 +184,7 @@
             this.Controls.Add(this.btn_ShowCreator);
             this.Controls.Add(this.btn_Play);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
